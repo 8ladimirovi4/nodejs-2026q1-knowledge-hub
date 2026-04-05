266840909566313a875fa80adfbb9cec7aa731de
Vladimir_Leonov <2227331.spb@gmail.com>
2026-04-05 19:39:48 +0300

    feat: create middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpContextMiddleware } from './common/middleware/http-context.middleware';
 import { UserModule } from './user/user.module';
 import { ArticleModule } from './article/article.module';
 
 @Module({
   imports: [UserModule, ArticleModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HttpContextMiddleware],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer
+      .apply(HttpContextMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
