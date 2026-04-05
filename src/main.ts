cb4fd2891ceadc3b98ffdf92283591acbf131686
Vladimir_Leonov <2227331.spb@gmail.com>
2026-04-05 21:43:25 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Knowledge Hub REST API')
+    .setDescription('OpenAPI documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
