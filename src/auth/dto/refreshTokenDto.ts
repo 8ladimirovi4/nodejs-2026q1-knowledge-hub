d9276d6ae7047e12adf21726959b14f1a9550341
Vladimir_Leonov <2227331.spb@gmail.com>
2026-04-19 21:22:53 +0300

    fix: refresh tocken dto

--- src/auth/dto/refreshTokenDto.ts
+++ src/auth/dto/refreshTokenDto.ts
@@ -1,12 +1,13 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class RefreshTokenDto {
-  @ApiProperty({
+  @ApiPropertyOptional({
     description: 'JWT refresh token',
     example: 'string',
   })
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
-  refreshToken: string;
+  refreshToken?: string;
 }
