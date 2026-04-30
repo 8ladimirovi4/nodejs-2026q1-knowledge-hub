8c215c1f61ffae864923d04016ae526636414040
Vladimir_Leonov <2227331.spb@gmail.com>
2026-04-30 17:56:36 +0300

    chor: edit eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,14 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js', 'test/**', '**/*.spec.ts'],
   rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
